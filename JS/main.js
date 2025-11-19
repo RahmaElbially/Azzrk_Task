60c0dd2f83e0238993bc0a716f334e5d76c54666
Rahma Elbially <rahmamohammed117@gmail.com>
2025-11-19 15:57:26 +0200

    add offers section and footer and responsive

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,31 @@ const mobileMenu = document.querySelector(".mobile-menu");
 burger.addEventListener("click", () => {
     mobileMenu.classList.toggle("show");
 });
+
+window.addEventListener("load", () => {
+    const slider = document.querySelector(".slider-container");
+    slider.scrollLeft = 150;
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener("mousedown", (e) => {
+        isDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    window.addEventListener("mouseup", () => {
+        isDown = false;
+    });
+
+    slider.addEventListener("mousemove", (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 1.2;
+        slider.scrollLeft = scrollLeft - walk;
+    });
+});
+
